ec6f1d1338439e52fd61c5251094f824778455bd
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-02-07 11:37:07 +0000

    logout view and url added and some imports for django

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, reverse, redirect
+from django.contrib import auth
 # Create your views here.
+
 
 def index(request):
     """ Returns the 'index.html' page """
     return render(request, 'index.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('index'))
