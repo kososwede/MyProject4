dd187bf68f607f227a6ed6075979ca9edc38fc5d
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-02-12 13:22:21 +0000

    added custom css and added a background image

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
     'accounts',
 ]
 
@@ -122,7 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_LOCATION = "static"
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
