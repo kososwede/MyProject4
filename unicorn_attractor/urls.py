c5aeb68ab54d59695598a34a4db5e6854d7705d8
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-02-12 10:24:31 +0000

    created new setting in settings.py 'EMAIL_BACKEND' to allow to send emails to the console

--- unicorn_attractor/urls.py
+++ unicorn_attractor/urls.py
@@ -19,7 +19,7 @@ from accounts import urls as accounts_urls
 from accounts.views import index
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),    
+    url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
     url(r'^accounts/', include(accounts_urls))
 ]
