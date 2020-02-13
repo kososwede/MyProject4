5073eed6dc5feb86ecce5ddf304162ba3935b8aa
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-02-13 15:58:23 +0000

    heroku app still not working, deleted from hostname

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -32,8 +32,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME', 'myproject-4.herokuapp.com')]
 
