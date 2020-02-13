009ad3cb461de41ade3a50d0a2086879ac55de8d
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-02-13 14:44:06 +0000

    added heroku to allowed hosts

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME'),
+                 os.environ.get('HEROKU_HOST')]
 
 
 # Application definition
