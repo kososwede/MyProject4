e3e9241beace9ee40c9b744c03f64d5e094a7f2a
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-02-14 11:46:10 +0000

    added AWS_DEFAULT_ACL = None in settings

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -148,8 +148,9 @@ AWS_STORAGE_BUCKET_NAME = 'mcilgrews-myproject4'
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = None
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = "/static/"
