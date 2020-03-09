3b6f0e207298ef3a180ffed32fda37a13e2e9d6d
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-03-09 19:39:57 +0000

    changed staticfiles storage back to what it was

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -151,7 +151,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
 
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
