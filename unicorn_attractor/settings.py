9c6c29960a0ebd7511e5fdd6fad0def55e689f1d
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-03-09 19:20:55 +0000

    added heroku app to allowed hosts

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = [os.environ.get('SECRET_KEY')]
 
 
-ALLOWED_HOSTS = [os.environ.get('GITPOD_HOSTNAME'), 'https://myproject-4.herokuapp.com/']
+ALLOWED_HOSTS = [os.environ.get('GITPOD_HOSTNAME'), 'myproject-4.herokuapp.com']
 
 
 # Application definition
