3dda3fdbb8b58f9d38ce78ce10bc14487169be83
Christopher McIlgrew <chrismcilgrew@icloud.com>
2020-03-31 10:48:51 +0000

    changed debug to false

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -16,10 +16,7 @@ from os import path
 if path.exists("env.py"):
   import env
 # Set debug to True only in test environment
-if 'HOSTNAME' in os.environ:
-    DEBUG = True
-else:
-    DEBUG = False
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
