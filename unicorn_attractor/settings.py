e737c54eebfc4a78464ce4a52d4724398cb9aab3
Christopher McIlgrew <c.mcilgrew1982@gmail.com>
2021-03-12 14:35:41 +0000

    add whitenoise

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -152,10 +152,10 @@ USE_TZ = True
 
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+#STATIC_URL = "/static/"
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, "static")
+#]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
