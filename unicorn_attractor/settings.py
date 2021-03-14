963f2c8426eea3dff4cbd53b74f023c0fb14783b
Christopher McIlgrew <c.mcilgrew1982@gmail.com>
2021-03-14 08:04:08 +0000

    added the .. to the begining of the static files

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -137,24 +137,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-AWS_S3_OBJECT_PARAMETERS = {
-    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=9460800',
-}
+#AWS_S3_OBJECT_PARAMETERS = {
+ #   'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+  #  'CacheControl': 'max-age=9460800',
+#}
 
-AWS_STORAGE_BUCKET_NAME = 'mcilgrews-myproject4'
-AWS_S3_REGION_NAME = 'eu-north-1'
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_DEFAULT_ACL = None
+#AWS_STORAGE_BUCKET_NAME = 'mcilgrews-myproject4'
+#AWS_S3_REGION_NAME = 'eu-north-1'
+#AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+#AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+#AWS_DEFAULT_ACL = None
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATIC_URL = "/static/"
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, "static")
+#]
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
