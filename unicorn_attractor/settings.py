b72168aaf0760578e7415480480a91e1c8b419fb
Christopher McIlgrew <c.mcilgrew1982@gmail.com>
2021-03-14 08:33:11 +0000

    un commented out static paths

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -16,7 +16,7 @@ from os import path
 if path.exists("env.py"):
     import env
 # Set debug to True only in test environment
-DEBUG = True
+DEBUG = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
