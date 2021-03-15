3851d812a282335ab2a73cd46949fd49763bebee
Christopher McIlgrew <c.mcilgrew1982@gmail.com>
2021-03-15 11:48:35 +0000

    reinstalled all in requirements.txt folder

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -150,7 +150,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
 
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
